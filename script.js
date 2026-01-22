ca8b7772df55e5035621557e78591609906bfe83
nestsoul719-max <nestsoul719@gmail.com>
2026-01-22 12:51:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const root = document.getElementById("root");
+
+root.innerHTML = `
+  <div style="
+    text-align:center;
+    padding:40px;
+    color:#4c1d95;
+  ">
+    <h1>🌙 Soul.Nest</h1>
+    <p>Your calm, safe space to talk.</p>
+  </div>
+`;
